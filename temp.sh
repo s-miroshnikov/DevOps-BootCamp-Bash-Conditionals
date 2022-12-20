732fd25f6fc67a8903df74e45e93e5e76a7fc405
s.miroshnikov <97443660+s-miroshnikov@users.noreply.github.com>
2022-12-20 22:56:07 +0400

    Changed echo output for results in C

--- temp.sh
+++ temp.sh
@@ -7,7 +7,7 @@ regex_C="^[0-9]+C$"
 # if we have temperature in Kelvin we use C = K - 273; if temperature was in Celsius - K = C + 273
 if [[ "${tempr}" =~ $regex_K ]]; then
   t_C=$(( "${tempr::-1}" - 273 ))
-  echo "$t_C"K
+  echo "$t_C"C
   elif [[ "${tempr}" =~ $regex_C ]]; then
   t_K=$(( "${tempr::-1}" + 273 ))
   echo "$t_K"K
