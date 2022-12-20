1f8951af5b2c9c256940728756b60e7be99a3f6c
s.miroshnikov <97443660+s-miroshnikov@users.noreply.github.com>
2022-12-20 12:54:48 +0400

    Update cond1.sh

--- cond1.sh
+++ cond1.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-IFS=',' read -r -a array <<< "$1"
-
-#Write your code here
+# we will use provided numbers as array, with ',' as field separator
+IFS=',' read -ra numarray <<< "$@"
+# for sum of even numbers we will use the evensum variable with an initial value of 0
+evensum=0
+# later, we sum each number which can be divided by 2 without a remainder (even)  
+for i in "${numarray[@]}"; do
+if [ $((i%2)) -eq 0 ]
+then
+  (( evensum+=i ))
+fi
+ done
+echo $evensum
