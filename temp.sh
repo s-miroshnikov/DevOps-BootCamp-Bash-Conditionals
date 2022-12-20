315d62b1d8045560b94a826b060e2a1f63a9143d
s.miroshnikov <97443660+s-miroshnikov@users.noreply.github.com>
2022-12-20 18:00:29 +0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-# Place your code here
+tempr=$1
+# for unit of measurement recognition, we will use 2 regex
+regex_K="^[0-9]+K$"
+regex_C="^[0-9]+C$"
+# if we have temperature in Kelvin we use C = K - 273; if temperature was in Celsius - K = C + 273
+if [[ "${tempr}" =~ $regex_K ]]; then
+  t_C=$(( "${tempr::-1}" - 273 ))
+  echo "$t_C"K
+  elif [[ "${tempr}" =~ $regex_C ]]; then
+  t_K=$(( "${tempr::-1}" + 273 ))
+  echo "$t_K"K
+  fi
